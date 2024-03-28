55916aab6fee722442836679e067f4f9d537d75d
arco_notebook <3481554467@qq.com>
2024-03-28 11:24:20 +0800

    pulsar_conf

--- gotimer_scheduler/mq/client.go
+++ gotimer_scheduler/mq/client.go
@@ -3,7 +3,9 @@ package mq
 import (
 	"fmt"
 	"github.com/apache/pulsar-client-go/pulsar"
+	"gopkg.in/yaml.v2"
 	"log"
+	"os"
 	"time"
 )
 
@@ -11,12 +13,32 @@ type PulsarClient struct {
 	Client pulsar.Client
 }
 
+type PulsarConfig struct {
+	URL string `yaml:"url"`
+}
+
 func GetPulsarClient() *PulsarClient {
+	configFile, err := os.ReadFile("conf.yml")
+	if err != nil {
+		log.Fatalf("Unable to read config file: %v", err)
+	}
+
+	// 定义配置变量
+	var config PulsarConfig
+
+	// 解析YAML配置
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatalf("Unable to unmarshal config file: %v", err)
+	}
+
+	// 使用解析出的URL创建Pulsar客户端
 	Client, err := pulsar.NewClient(pulsar.ClientOptions{
-		URL:               "pulsar://10.9.130.50:6650",
+		URL:               config.URL,
 		OperationTimeout:  30 * time.Second,
 		ConnectionTimeout: 30 * time.Second,
 	})
+
 
 	if err != nil {
 		log.Fatalf("Could not instantiate Pulsar client: %v", err)
